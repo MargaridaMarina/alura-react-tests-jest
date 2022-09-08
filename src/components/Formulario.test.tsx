ae6a1c785a03e048dc7f11e2e4aba81a79f69406
Margarida <margaridammcm@gmail.com>
2022-09-08 18:38:34 -0300

    Create test

--- src/components/Formulario.test.tsx
+++ src/components/Formulario.test.tsx
@@ -1,11 +1,36 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import Formulario from "./Formulario";
 
 test('Quando o input está vazio, novos participantes não podem ser adicionados', () => {
   render(<Formulario/>)
+  // encontrar no DOM o input
   const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+  // encontrar o botao
   const botao = screen.getByRole('button')
+  // garantir que o input esteja no documento
   expect(input).toBeInTheDocument()
+  // garantir que o botao esteja desabilitado
   expect(botao).toBeDisabled()
+})
+
+test('adicionar um participante caso exista um nome preenchido', () => {
+  render(<Formulario/>)
+   // encontrar no DOM o input
+   const input = screen.getByPlaceholderText('Insira os nomes dos participantes')
+   // encontrar o botao
+   const botao = screen.getByRole('button')
+   // inserir um valor no input
+   fireEvent.change(input, {
+    target: {
+      value: 'Ana Catarina'
+    }
+   })
+   // clicar no botao submeter
+   fireEvent.click(botao)
+   // garantir que o input esteja com o foco ativo
+   expect(input).toHaveFocus()
+   // garantir que o input não tenha um valor
+   expect(input).toHaveValue("")
+
 })
